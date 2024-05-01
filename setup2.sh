06d9980695876e45eecb8cdda411bf3815b23a49
Mads Rossen <56363524+MadsRossen@users.noreply.github.com>
2024-05-01 11:46:00 +0200

    Setup inside docker compose file

---
+++ setup2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo dpkg -i ftp.evocortex.com/libirimager-8.10.1-arm64.deb 
+
+cd /workspaces/isaac_ros-dev/ && \
+    rosdep install -i -r --from-paths src --rosdistro humble -y --skip-keys "libopencv-dev libopencv-contrib-dev libopencv-imgproc-dev python-opencv python3-opencv nvblox"
+
+cd /workspaces/isaac_ros-dev && \
+  colcon build --symlink-install && \
+  source install/setup.bash
