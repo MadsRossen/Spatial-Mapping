c324d43857663a8732b1fb26fbd78a63df151c82
Mads Rossen <56363524+MadsRossen@users.noreply.github.com>
2024-05-02 16:50:54 +0200

    Removing a specific x86_64 version

--- setup2.sh
+++ setup2.sh
@@ -3,12 +3,14 @@ sudo apt update
 sudo apt install libudev-dev
 
 arch=$(uname -i)
-if [[ $arch == x86_64* ]]; then
-    cd /workspaces/isaac_ros-dev/ && \
-        sudo dpkg -i ftp.evocortex.com/libirimager-8.10.1-amd64.deb
+if [[ $arch == x86_64 ]]; then
+  echo 'This is x86_64 Architecture'
+  cd /workspaces/isaac_ros-dev/ && \
+      sudo dpkg -i ftp.evocortex.com/libirimager-8.10.1-amd64.deb
 elif [[ $arch == aarch64 ]]; then
-    cd /workspaces/isaac_ros-dev/ && \
-        sudo dpkg -i ftp.evocortex.com/libirimager-8.10.1-arm64.deb 
+  echo 'This is ARM Architecture'
+  cd /workspaces/isaac_ros-dev/ && \
+      sudo dpkg -i ftp.evocortex.com/libirimager-8.10.1-arm64.deb 
 fi
 
 cd /workspaces/isaac_ros-dev/ && \
