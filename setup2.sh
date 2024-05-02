4c697e72b81bef757e3bedd64b74a0a556769eef
Mads Rossen <56363524+MadsRossen@users.noreply.github.com>
2024-05-02 16:43:02 +0200

    multiple achritectures

--- setup2.sh
+++ setup2.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 sudo apt update
 sudo apt install libudev-dev
-cd /workspaces/isaac_ros-dev/ && \
-    sudo dpkg -i ftp.evocortex.com/libirimager-8.10.1-arm64.deb 
+
+arch=$(uname -i)
+if [[ $arch == x86_64* ]]; then
+    cd /workspaces/isaac_ros-dev/ && \
+        sudo dpkg -i ftp.evocortex.com/libirimager-8.10.1-amd64.deb
+elif [[ $arch == aarch64 ]]; then
+    cd /workspaces/isaac_ros-dev/ && \
+        sudo dpkg -i ftp.evocortex.com/libirimager-8.10.1-arm64.deb 
+fi
 
 cd /workspaces/isaac_ros-dev/ && \
     rosdep install -i -r --from-paths src --rosdistro humble -y --skip-keys "libopencv-dev libopencv-contrib-dev libopencv-imgproc-dev python-opencv python3-opencv nvblox"
