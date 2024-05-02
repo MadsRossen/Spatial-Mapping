ac3d4c4717eafcab4660092c3407bd6cbb10b0f2
Mads Rossen <56363524+MadsRossen@users.noreply.github.com>
2024-05-02 16:17:47 +0200

    work directory update

--- setup2.sh
+++ setup2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+sudo apt update
 sudo apt install libudev-dev
-
-sudo dpkg -i ftp.evocortex.com/libirimager-8.10.1-arm64.deb 
+cd /workspaces/isaac_ros-dev/ && \
+    sudo dpkg -i ftp.evocortex.com/libirimager-8.10.1-arm64.deb 
 
 cd /workspaces/isaac_ros-dev/ && \
     rosdep install -i -r --from-paths src --rosdistro humble -y --skip-keys "libopencv-dev libopencv-contrib-dev libopencv-imgproc-dev python-opencv python3-opencv nvblox"
