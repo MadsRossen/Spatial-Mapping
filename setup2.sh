7a92060c73ed424cca00dc2d513ab3c772e3f00c
Mads Rossen <56363524+MadsRossen@users.noreply.github.com>
2024-05-02 23:34:25 +0200

    added space

--- setup2.sh
+++ setup2.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install libudev-dev
 
 arch=$(uname -m)
-if [$arch == "x86_64" ]; then
+if [ $arch == "x86_64" ]; then
   echo 'This is x86_64 Architecture'
   cd /workspaces/isaac_ros-dev/ && \
       sudo dpkg -i ftp.evocortex.com/libirimager-8.10.1-amd64.deb
