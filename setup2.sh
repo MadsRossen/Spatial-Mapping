fa30d50c37ce1552213cfef415bf4fd81d9e1f0c
Mads Rossen <56363524+MadsRossen@users.noreply.github.com>
2024-05-02 23:42:28 +0200

    Added BOLD letters

--- setup2.sh
+++ setup2.sh
@@ -2,15 +2,17 @@
 sudo apt update
 sudo apt install libudev-dev
 
-arch=$(uname -m)
-if [ "$arch" == "x86_64" ]; then
+ARCH=$(uname -m)
+if [ "$ARCH" == "x86_64" ]; then
   echo 'This is x86_64 Architecture'
   cd /workspaces/isaac_ros-dev/ && \
       sudo dpkg -i ftp.evocortex.com/libirimager-8.10.1-amd64.deb
-elif [ "$arch" == "aarch64" ]; then
+elif [ "$ARCH" == "aarch64" ]; then
   echo 'This is ARM Architecture'
   cd /workspaces/isaac_ros-dev/ && \
       sudo dpkg -i ftp.evocortex.com/libirimager-8.10.1-arm64.deb 
+else
+  echo "Unsupported OS"
 fi
 
 cd /workspaces/isaac_ros-dev/ && \
