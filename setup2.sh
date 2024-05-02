4ba55d19ef623fe4460e1da8658fe8db90a64fcc
Mads Rossen <56363524+MadsRossen@users.noreply.github.com>
2024-05-02 23:27:41 +0200

    no string

--- setup2.sh
+++ setup2.sh
@@ -3,12 +3,12 @@ sudo apt update
 sudo apt install libudev-dev
 
 arch=$(uname -m)
-if [$arch == 'x86_64' ] 
+if [$arch == x86_64 ] 
   then
   echo 'This is x86_64 Architecture'
   cd /workspaces/isaac_ros-dev/ && \
       sudo dpkg -i ftp.evocortex.com/libirimager-8.10.1-amd64.deb
-elif [ $arch == 'aarch64' ] 
+elif [ $arch == aarch64 ] 
   then
   echo 'This is ARM Architecture'
   cd /workspaces/isaac_ros-dev/ && \
